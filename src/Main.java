1b3bc4fbb3d48663a4204ecf4ff4573736122c6a
prateekch33 <prateekchandra27@outlook.com>
2023-12-06 20:29:48 +0530

    Refactor 1 using Generics

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,8 @@
 import java.util.Scanner;
 
 public class Main {
-    public static int max(int a,int b,int c) {
-        Integer x1= a;
-        Integer x2=b;
-        Integer x3=c;
-        if(x1.compareTo(x2)>0 && x1.compareTo(x3)>0) {
-            return a;
-        } else if (x2.compareTo(x1)>0 && x2.compareTo(x3)>0){
-            return b;
-        } else {
-            return c;
-        }
-    }
-    public static double max(double a,double b,double c) {
-        Double x1= a;
-        Double x2=b;
-        Double x3=c;
-        if(x1.compareTo(x2)>0 && x1.compareTo(x3)>0) {
-            return a;
-        } else if (x2.compareTo(x1)>0 && x2.compareTo(x3)>0){
-            return b;
-        } else {
-            return c;
-        }
-    }
-    public static String max(String a,String b,String c) {
 
+    public static <T extends Comparable<T>> T max(T a,T b,T c) {
         if(a.compareTo(b)>0 && a.compareTo(c)>0) {
             return a;
         } else if (b.compareTo(a)>0 && b.compareTo(c)>0){
@@ -37,11 +13,47 @@ public class Main {
     }
     public static void main(String[] args) {
         Scanner in=new Scanner(System.in);
-        System.out.println("Enter 3 Strings.");
-        String a=in.nextLine();
-        String b=in.nextLine();
-        String c=in.nextLine();
-        System.out.printf("Maximum among all the 3 Strings.: %s\n",max(a,b,c));
+        int choice;
+        do{
+            System.out.println("Choose any one of the following:");
+            System.out.println("1. Integer");
+            System.out.println("2. Floating No.");
+            System.out.println("3. Strings");
+            System.out.println("4. Exit");
+            choice=in.nextInt();
+            switch(choice) {
+                case 1: {
+                    System.out.println("Enter 3 Integers.");
+                    Integer a=in.nextInt();
+                    Integer b=in.nextInt();
+                    Integer c=in.nextInt();
+                    System.out.printf("Maximum among all the 3 Integers: %s\n",max(a,b,c));
+                    break;
+                }
+                case 2: {
+                    System.out.println("Enter 3 Floating No.");
+                    Double a=in.nextDouble();
+                    Double b=in.nextDouble();
+                    Double c=in.nextDouble();
+                    System.out.printf("Maximum among all the 3 Integers: %s\n",max(a,b,c));
+                    break;
+                }
+                case 3: {
+                    System.out.println("Enter 3 Strings.");
+                    String a=in.nextLine();
+                    String b=in.nextLine();
+                    String c=in.nextLine();
+                    System.out.printf("Maximum among all the 3 Integers: %s\n",max(a,b,c));
+                    break;
+                }
+                case 4:
+                    break;
+                default:
+                    System.out.println("Wrong Choice!!");
+                    break;
+            }
+        } while(choice!=4);
+        System.out.println("Thank you!!");
 
         in.close();
     }
